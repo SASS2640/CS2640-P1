a221da3c34da0b1eb4356f6b5c171ceabd2f14df
SarahAHuynh <shuynh1@outlook.com>
2024-11-01 15:11:58 -0700

    Finished Task1: get 2 ints from user and output them (Sayumi)

--- p1.asm
+++ p1.asm
@@ -1,5 +1,5 @@
 #CS 2640
-#Sarah Huynh, Ashley Mapes, Sayumi Amarasinghe, Samuel Ji
+#Group members: Sarah Huynh, Ashley Mapes, Sayumi Amarasinghe, Samuel Ji
 #11/01/2024
 #Program 1: Getting Familiar with Assembly
 # 1) gets 2 ints from user 
@@ -9,3 +9,52 @@
 # 5) print results of operations to the user
 # 6) practice conditions
 # 7) exit program 
+
+.data 
+prompt1: .asciiz "Please enter a first integer value: "
+prompt2: .asciiz "Please enter a second integer value: "
+newline: .asciiz "\n"
+
+.text
+main:
+    # print out prompt 
+    li $v0, 4
+    la $a0, prompt1
+    syscall
+    
+    #get first integer 
+     li $v0, 5
+     syscall
+     move $s0, $v0
+
+    #print second prompt 
+     li $v0, 4
+     la $a0, prompt2
+     syscall
+     
+     #get second integer 
+     li $v0, 5
+     syscall
+     move $s1, $v0
+     
+     
+     #print first integer
+     li $v0, 1
+     move $a0, $s0
+     syscall
+     
+     #print newline 
+     li $v0, 4
+     la $a0, newline
+     syscall
+     
+     
+     #print second integer
+     li $v0, 1
+     move $a0 ,$s1
+     syscall 
+        
+    
+    #exit syscall
+    li $v0, 10
+    syscall
