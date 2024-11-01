46f124b58a239f8876da3b3d270d8f426cb73649
SarahAHuynh <shuynh1@outlook.com>
2024-11-01 12:19:26 -0700

    setup program

---
+++ p1.asm
@@ -0,0 +1,11 @@
+#CS 2640
+#Sarah Huynh, Ashley Mapes, Sayumi Amarasinghe, Samuel Ji
+#11/01/2024
+#Program 1: Getting Familiar with Assembly
+# 1) gets 2 ints from user 
+# 2) move them to registers
+# 3) print them back to the user
+# 4) complete arithmetic operations to the 2 ints
+# 5) print results of operations to the user
+# 6) practice conditions
+# 7) exit program 
