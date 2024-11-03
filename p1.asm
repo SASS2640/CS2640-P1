ee5a0e832e0976ea7ac95c895e2f962d54655527
SarahAHuynh <shuynh1@outlook.com>
2024-11-02 22:28:17 -0700

    Implemented conditions for user input (Ashley)

--- p1.asm
+++ p1.asm
@@ -19,128 +19,164 @@ prompt5: .asciiz "Result of addition between the two numbers is: "
 prompt6: .asciiz "Result of subtraction between the two numbers is: " 
 prompt7: .asciiz "Result of multiplication between the two numbers is: " 
 prompt8: .asciiz "Result of division between the two numbers is: "
+equal_message:	.asciiz "User inputs are the same"
+not_equal_message: .asciiz "User inputs are different"
 newline: .asciiz "\n" 
+
+.macro exit
+		li $v0, 10
+		syscall
+.end_macro
 
 .text
 main:
-    # print out prompt1 
-    li $v0, 4
-    la $a0, prompt1
-    syscall
+	# PART 1
+    	# print out prompt1 
+    	li $v0, 4
+    	la $a0, prompt1
+    	syscall
     
-    #get first integer 
-     li $v0, 5
-     syscall
-     move $s0, $v0
+    	# get first integer 
+     	li $v0, 5
+     	syscall
+     	move $s0, $v0
 
-    #print second prompt2
-     li $v0, 4
-     la $a0, prompt2
-     syscall
+    	# print second prompt2
+     	li $v0, 4
+     	la $a0, prompt2
+     	syscall
      
-     #get second integer 
-     li $v0, 5
-     syscall
-     move $s1, $v0
+     	# get second integer 
+     	li $v0, 5
+     	syscall
+     	move $s1, $v0
      
-     # print out prompt3
-     li $v0, 4
-     la $a0, prompt3
-     syscall
+     	# print out prompt3
+     	li $v0, 4
+     	la $a0, prompt3
+     	syscall
      
-     #print first integer
-     li $v0, 1
-     move $a0, $s0
-     syscall
+     	# print first integer
+     	li $v0, 1
+     	move $a0, $s0
+     	syscall
      
-     #print newline 
-     li $v0, 4
-     la $a0, newline
-     syscall
+     	# print newline 
+     	li $v0, 4
+     	la $a0, newline
+     	syscall
      
-     # print out prompt4
-     li $v0, 4
-     la $a0, prompt4
-     syscall
+     	# PART 2
+     	# print out prompt4
+     	li $v0, 4
+     	la $a0, prompt4
+     	syscall
      
-     #print second integer
-     li $v0, 1
-     move $a0 ,$s1
-     syscall 
+     	# print second integer
+     	li $v0, 1
+     	move $a0 ,$s1
+     	syscall 
      
-     #print newline 
-     li $v0, 4
-     la $a0, newline
-     syscall
+     	# print newline 
+     	li $v0, 4
+     	la $a0, newline
+     	syscall
       
-     # add two integers 
-     add $s2, $s0, $s1
+     	# add two integers 
+     	add $s2, $s0, $s1
      
-     # print out prompt5
-     li $v0, 4
-     la $a0, prompt5
-     syscall
+     	# print out prompt5
+     	li $v0, 4
+     	la $a0, prompt5
+     	syscall
      
-     # print out the result of the addition 
-     li $v0, 1
-     move $a0, $s2
-     syscall
+     	# print out the result of the addition 
+     	li $v0, 1
+     	move $a0, $s2
+     	syscall
      
-     # print new line
-     li $v0, 4
-     la $a0, newline
-     syscall
+     	# print new line
+     	li $v0, 4
+     	la $a0, newline
+     	syscall
      
-     # subtract two integers 
-     sub $s2, $s0, $s1
+     	# subtract two integers 
+     	sub $s2, $s0, $s1
      
-     # print out prompt6
-     li $v0, 4
-     la $a0, prompt6
-     syscall
+     	# print out prompt6
+     	li $v0, 4
+     	la $a0, prompt6
+     	syscall
      
-     # print out the result of subtraction 
-     li $v0, 1
-     move $a0, $s2
-     syscall
+     	# print out the result of subtraction 
+     	li $v0, 1
+     	move $a0, $s2
+     	syscall
      
-     # print new line
-     li $v0, 4
-     la $a0, newline
-     syscall
+     	# print new line
+     	li $v0, 4
+     	la $a0, newline
+     	syscall
      
-     #mutiply two integers
-     mul $s2, $s0, $s1 
+     	# mutiply two integers
+     	mul $s2, $s0, $s1 
      
-     # print out prompt7
-     li $v0, 4
-     la $a0, prompt7
-     syscall
+     	# print out prompt7
+     	li $v0, 4
+     	la $a0, prompt7
+     	syscall
      
-     #print out the result of the multiplication
-     li $v0, 1 
-     move $a0, $s2 
-     syscall 
+     	# print out the result of the multiplication
+     	li $v0, 1 
+     	move $a0, $s2 
+     	syscall 
      
-     #print newline 
-     li $v0, 4
-     la $a0, newline
-     syscall
+     	# print newline 
+     	li $v0, 4
+     	la $a0, newline
+     	syscall
      
-     #division 
-     div $s0, $s1 
-     mflo $s2 
+     	# division 
+     	div $s0, $s1 
+     	mflo $s2 
      
-     # print out prompt8 
-     li $v0, 4
-     la $a0, prompt8
-     syscall
+     	# print out prompt8 
+     	li $v0, 4
+     	la $a0, prompt8
+     	syscall
     
-     #print out the result of the divison
-     li $v0, 1 
-     move $a0, $s2
-     syscall 
-    
-     #exit syscall
-     li $v0, 10
-     syscall
+     	# print out the result of the divison
+     	li $v0, 1 
+     	move $a0, $s2
+     	syscall 
+     
+    	# PART 3
+    	# if the two user inputs from Task 1 are equal to each other, output "User inputs are the same"
+    	# if the two user inputs from Task 1 are not equal to each other, output "User inputs are different"
+	#print newline 
+     	li $v0, 4
+     	la $a0, newline
+     	syscall
+     	
+     	# compare if equal to
+	beq $s0, $s1, equal
+	#compare if not equal to
+	bne $s0, $s1, not_equal
+	
+equal:
+	# print equal message
+	li $v0, 4
+	la $a0, equal_message
+	syscall
+	
+	# call exit macro
+	exit
+	
+not_equal:
+	# print not equal message
+	li $v0, 4
+	la $a0, not_equal_message
+	syscall
+	
+	# call exit macro
+	exit
